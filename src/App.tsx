f553804dcc7fcf350a1b89e5c7bb2bbfb07f8eda
Vu Minh Le <vuminhle@outlook.com>
2024-06-14 11:46:04 +0200

    remove unnessary code fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
